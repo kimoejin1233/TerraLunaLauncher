e14531b06c12f1d11df6ff2a2bcf8713ea60d053
kimoejin1233 <82705987+kimoejin1233@users.noreply.github.com>
2024-11-05 06:39:11 +0900

    초기테스트

--- app/assets/js/serverstatus.js
+++ app/assets/js/serverstatus.js
@@ -1,65 +1,51 @@
-const net = require('net')
+import { connect } from 'net';
 
 /**
- * Retrieves the status of a minecraft server.
- * 
+ * Retrieves the status of a Minecraft server.
+ *
  * @param {string} address The server address.
  * @param {number} port Optional. The port of the server. Defaults to 25565.
  * @returns {Promise.<Object>} A promise which resolves to an object containing
  * status information.
  */
-exports.getStatus = function(address = '118.220.140.108', port = 8080){
-
-    if(port == null || port == ''){
-        port = 25565
-    }
-    if(typeof port === 'string'){
-        port = parseInt(port)
-    }
+export function getStatus(address = '118.220.140.108', port = 25565) {
+    // Ensure port is a number
+    port = typeof port === 'string' ? parseInt(port, 10) : port;
 
     return new Promise((resolve, reject) => {
-        const socket = net.connect(port, address, () => {
-            let buff = Buffer.from([0xFE, 0x01])
-            socket.write(buff)
-        })
+        const socket = connect(port, address, () => {
+            const buff = Buffer.from([0xFE, 0x01]);
+            socket.write(buff);
+        });
 
         socket.setTimeout(2500, () => {
-            socket.end()
-            reject({
-                code: 'ETIMEDOUT',
-                errno: 'ETIMEDOUT',
-                address,
-                port
-            })
-        })
+            socket.end();
+            reject({ code: 'ETIMEDOUT', errno: 'ETIMEDOUT', address, port });
+        });
 
         socket.on('data', (data) => {
-            if(data != null && data != ''){
-                let server_info = data.toString().split('\x00\x00\x00')
-                const NUM_FIELDS = 6
-                if(server_info != null && server_info.length >= NUM_FIELDS){
+            if (data && data.length) {
+                const serverInfo = data.toString().split('\x00\x00\x00');
+                const NUM_FIELDS = 6;
+
+                if (serverInfo.length >= NUM_FIELDS) {
                     resolve({
                         online: true,
-                        version: server_info[2].replace(/\u0000/g, ''),
-                        motd: server_info[3].replace(/\u0000/g, ''),
-                        onlinePlayers: server_info[4].replace(/\u0000/g, ''),
-                        maxPlayers: server_info[5].replace(/\u0000/g,'')
-                    })
+                        version: serverInfo[2].replace(/\u0000/g, ''),
+                        motd: serverInfo[3].replace(/\u0000/g, ''),
+                        onlinePlayers: parseInt(serverInfo[4].replace(/\u0000/g, ''), 10),
+                        maxPlayers: parseInt(serverInfo[5].replace(/\u0000/g, ''), 10),
+                    });
                 } else {
-                    resolve({
-                        online: false
-                    })
+                    resolve({ online: false });
                 }
             }
-            socket.end()
-        })
+            socket.end();
+        });
 
         socket.on('error', (err) => {
-            socket.destroy()
-            reject(err)
-            // ENOTFOUND = Unable to resolve.
-            // ECONNREFUSED = Unable to connect to port.
-        })
-    })
-
+            socket.destroy();
+            reject(err);
+        });
+    });
 }
